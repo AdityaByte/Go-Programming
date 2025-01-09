f88ea749a119e77ec2e193f87c10f6df7df85245
Aditya-Byte <adityapawar4914@gmail.com>
2025-01-09 12:12:07 +0530

    Http verb Post in golang

--- 21webrequestsverb/main.go
+++ 21webrequestsverb/main.go
@@ -9,13 +9,15 @@ import (
 
 func main() {
 	fmt.Println("Web Requests Verb in golang")
-	HandlingGetRequest()
+	// PerformGetRequest()
+	PerformJsonPostRequest()
 }
 
-func HandlingGetRequest() {
-	const url string = "http://localhost:8080/api/get"
+const url string = "http://localhost:8080/api"
 
-	response, err := http.Get(url)
+func PerformGetRequest() {
+
+	response, err := http.Get(url + "/get")
 
 	if err != nil {
 		panic(err)
@@ -41,5 +43,44 @@ func HandlingGetRequest() {
 	byteCount, _ := responseString.Write(content) // This will take the content and write it.
 
 	fmt.Println("byte count is", byteCount)
+	fmt.Println(responseString.String())
+}
+
+func PerformJsonPostRequest() {
+
+	// This is not the genuine way of creating json but it is one of the way.
+	requestBody := strings.NewReader(`
+		{
+			"id": 1234,
+			"username": "aditya",
+			"website": "aditya.go.dev"
+		}
+	`)
+
+	// Here we are creating the Post request to the javaAPI
+	response, err := http.Post(url+"/post", "application/json", requestBody)
+
+	
+	if err != nil {
+		panic(err)
+	}
+	
+	// Make sure you do that it's your responsibility.
+	defer response.Body.Close()
+	
+	// Reading the content from the body
+	content, _ := io.ReadAll(response.Body)
+
+	// strings.Builder is a struct in the strings package
+	// Here we are creating an instance of the Builder struct.
+	// Builder acts as a mutable buffer where you can write content to make a string rather than creating multiple copies.
+
+	var responseString strings.Builder
+
+	byteCount, _ := responseString.Write(content)
+
+	fmt.Println("Byte count is", byteCount)
+
+	// Since the Builder is a mutable object we can return the instance here.
 	fmt.Println(responseString.String())
 }
